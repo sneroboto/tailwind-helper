f239a3e006c1c6a4bc53bda2b8a367cf64a87e00
sneroboto <sne@roboto.studio>
2025-03-09 18:45:02 +0530

    Update border color to purple-800 in Card component

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ title, text }: { title: string; text: string }) {
   return (
-    <section className="bg-purple-500 p-4 border border-amber-800">
+    <section className="bg-purple-500 p-4 border border-purple-800">
       <h2 className="text-xl font-bold">{title}</h2>
       <p>{text}</p>
     </section>
