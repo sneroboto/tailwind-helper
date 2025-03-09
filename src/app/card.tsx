eddea686eba4cd5c67f75d7318e63b35af7b07cc
sneroboto <sne@roboto.studio>
2025-03-09 18:43:32 +0530

    Hello world - intii

---
+++ src/app/card.tsx
@@ -0,0 +1,8 @@
+export default function Card({ title, text }: { title: string; text: string }) {
+  return (
+    <section className="bg-purple-500 p-4 border border-amber-800">
+      <h2 className="text-xl font-bold">{title}</h2>
+      <p>{text}</p>
+    </section>
+  );
+}
